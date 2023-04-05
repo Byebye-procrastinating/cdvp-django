0655de155c214eef3b28730502ccb372ca08d9d7
LGLJ <1084737354@qq.com>
2023-04-05 16:42:45 +0800

    Update docker_eni.py

--- visualize/docker_engine/docker_eni.py
+++ visualize/docker_engine/docker_eni.py
@@ -1,11 +1,12 @@
 import os
-import build_for_py
-import build_for_cpp
+
+from .build_for_py import build_docker_for_py
+from .build_for_cpp import build_docker_for_cpp
 
 
 def get_output(filename):
     extension = os.path.splitext(filename)[1]
     if extension == '.py':
-        return build_for_py.build_docker_for_py(filename)
+        return build_docker_for_py(filename)
     elif extension == '.cpp' or extension == '.c':
-        return build_for_cpp.build_docker_for_cpp(filename, extension)
+        return build_docker_for_cpp(filename, extension)
